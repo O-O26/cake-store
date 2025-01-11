e960a19edacd59f7d202fe0e32f49ac0c315fdf9
Ola Osoba <oladipupo.osoba26@gmail.com>
2025-01-11 14:16:21 +0000

    Pushing all current files to github

--- src/Components/product-card/product-card.component.jsx
+++ src/Components/product-card/product-card.component.jsx
@@ -1,19 +1,26 @@
+import { useContext } from "react"
+import { CartContext } from "../../../cake-store/src/contexts/cart.context"
 import "./product-card.styles.scss"
-
 import Button from "../button/button.component"
 
 const ProductCard = ({ product }) => {
     const { name, price, id, imageUrl } = product
+    const { addItemToCart } = useContext(CartContext)
+
+    const addProductToCart = () => addItemToCart(product);
+
     return (
-        <div className="product-card-container" key={id}>
-            <img src={imageUrl} alt={`${name}`} />
-            <div className="footer">
-                <span className="name">{name}</span>
-                <span className="price">{price}</span>
-            </div>
-            <Button buttonType="inverted">Add to cart</Button>
+      <div className='product-card-container'>
+        <img src={imageUrl} alt={`${name}`} />
+        <div className='footer'>
+          <span className='name'>{name}</span>
+          <span className='price'>{price}</span>
         </div>
-    )
+        <Button buttonType='inverted' onClick={addProductToCart}>
+          Add to card
+        </Button>
+      </div>
+    );
 }
 
 export default ProductCard
