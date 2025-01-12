0714e06c054c994bfe6f49c7237c240864b2e800
Ola Osoba <oladipupo.osoba26@gmail.com>
2025-01-12 15:59:24 +0000

    Added the addToCart feature on all products

--- src/Components/cart-icon/cart-icon.component.jsx
+++ src/Components/cart-icon/cart-icon.component.jsx
@@ -5,7 +5,7 @@ import ShoppingIcon from "../../assets/shopping-bag.svg"
 import "./cart-icon.styles.scss"
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext)
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext)
 
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
         // This function reverses the isCartOpen value so if isCartOpen
@@ -15,7 +15,7 @@ const CartIcon = () => {
         <div className="cart-icon-container" onClick={toggleIsCartOpen}>
             <img src={ShoppingIcon} alt="Shopping icon" 
             className="shopping-icon"/>
-            <span className="item-count">0</span>
+            <span className="item-count">{cartCount}</span>
         </div>
     )
 }
